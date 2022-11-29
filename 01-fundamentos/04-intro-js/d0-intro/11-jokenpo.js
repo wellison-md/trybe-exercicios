5cf0b8b13a34f26f68650e09fa5a49e4f6ea38c2
Wellison M. Dias <wellisonmdias@gmail.com>
2022-11-29 11:29:20 -0300

    concluído ex11

--- 01-fundamentos/04-intro-js/d0-intro/11-jokenpo.js
+++ 01-fundamentos/04-intro-js/d0-intro/11-jokenpo.js
@@ -9,16 +9,45 @@ const randomNumber = () => {
 }
 
 const initGame = () => {
-  let result = '';
   player1 = moves[randomNumber()];
   player2 = moves[randomNumber()];
   
   if(player1 === player2){
-    return result ='A Ties';
+    return 'A Ties';
   }
   if(player1 === 'rocks'){
-    if(player2 == ''){}
+    if(player2 === 'scisor'){
+      return 'Player 1 won';
+    }else{
+      return 'Player 2 won';
+    }
+  }
+
+  if(player1 === 'paper'){
+    if(player2 === 'rocks'){
+      return 'Player 1 won';
+    }else{
+      return 'Player 2 won';
+    }
+  }
+
+  if(player1 === 'scisor'){
+    if(player2 === 'paper'){
+      return 'Player 1 won';
+    }else{
+      return 'Player 2 won';
+    }
   }
 
 }
 
+const printResultGame = (result) => {
+  console.clear();
+  console.log(`+------------------+`);
+  console.log(`|       joKenPo    |`);
+  console.log(`+------------------+`);
+  console.log(`you: ${player1}\t\tplayer2: ${player2}`);
+  console.log(`>>> ${result}`);
+}
+
+printResultGame(initGame())
