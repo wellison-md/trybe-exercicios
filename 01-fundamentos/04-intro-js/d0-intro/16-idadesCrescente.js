c8bad3df7eb99c4e3b90ff8561ef010a03a5ea0c
Wellison M. Dias <wellisonmdias@gmail.com>
2022-11-30 20:29:10 -0300

    concluído ex16

---
+++ 01-fundamentos/04-intro-js/d0-intro/16-idadesCrescente.js
@@ -0,0 +1,11 @@
+//Crie um algoritmo que recebe a idade de Marina, Silvia e Iza e imprime as idades em ordem crescente.
+
+function crescentAges(marinaAge, silviaAge, izaAge){
+  let ages = [];
+  ages.push(marinaAge, silviaAge, izaAge);
+
+  ages.sort((a,b) => {return a - b})
+  return ages;
+}
+
+console.log(crescentAges(22,18,40));
