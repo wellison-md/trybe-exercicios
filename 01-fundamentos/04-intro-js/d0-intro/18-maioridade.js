1a784909c5ba1ca686ac61f728076a78f86a9491
Wellison M. Dias <wellisonmdias@gmail.com>
2022-11-30 22:09:34 -0300

    concluído ex18

---
+++ 01-fundamentos/04-intro-js/d0-intro/18-maioridade.js
@@ -0,0 +1,18 @@
+/* Desenvolva um algoritmo que verifica se a pessoa é maior ou menor de idade.
+Imprima no console seguindo o exemplo: "A pessoa é maior de idade". */
+
+function checkAge(age){
+  let personAge = Math.floor(age);
+  if (personAge >= 18) {
+    console.log('A pessoa é maior de idade');
+  }else{
+    console.log('A pessoa é menor de idade')
+  }
+}
+
+checkAge(15);
+checkAge(18);
+checkAge(35);
+checkAge(-12);
+checkAge(-23);
+checkAge(13.5);
