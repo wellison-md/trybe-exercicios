3162a3bc84968fcba0277cd14d5ac124e8cf657d
Wellison M. Dias <wellisonmdias@gmail.com>
2022-12-01 23:44:03 -0300

    update formatação ex7

--- 01-fundamentos/04-intro-js/d1-introducao-js/ex7.js
+++ 01-fundamentos/04-intro-js/d1-introducao-js/ex7.js
@@ -11,23 +11,23 @@ Porcentagem < 50 -> F
 O código deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100.
 */
 
-const grades = [30, 45, 70, 47, 69, 33, 178, -12, 87.5, 55, 76,92, 85]
+const grades = [30, 45, 70, 47, 69, 33, 178, -12, 87.5, 55, 76, 92, 85]
 
 const classifyGrades = (arrGrades) => {
   for (let key in arrGrades) {
     if (arrGrades[key] < 0 || arrGrades[key] > 100) {
-      console.log (`Erro! a nota ${arrGrades[key]} é inválida!`);
+      console.log(`Erro! a nota ${arrGrades[key]} é inválida!`);
     } else if (arrGrades[key] >= 90) {
       console.log(arrGrades[key], ' A');
-    }else if( arrGrades[key] >= 80 && arrGrades[key] < 90){
+    } else if (arrGrades[key] >= 80 && arrGrades[key] < 90) {
       console.log(arrGrades[key], ' B');
-    }else if(arrGrades[key] >= 70 && arrGrades[key] < 80){
+    } else if (arrGrades[key] >= 70 && arrGrades[key] < 80) {
       console.log(arrGrades[key], ' C');
-    }else if(arrGrades[key] >= 60 && arrGrades[key] < 70){
+    } else if (arrGrades[key] >= 60 && arrGrades[key] < 70) {
       console.log(arrGrades[key], ' D');
-    }else if(arrGrades[key] >= 50 && arrGrades[key] < 60){
+    } else if (arrGrades[key] >= 50 && arrGrades[key] < 60) {
       console.log(arrGrades[key], ' E');
-    }else if(arrGrades[key] < 50){
+    } else if (arrGrades[key] < 50) {
       console.log(arrGrades[key], ' F');
     }
   }
